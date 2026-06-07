f661435248c30d16d37c3620af94a4f3b11be648
Fábio Luís <ml@fabiotecnico1.biz>
2026-06-07 14:10:26 -0300

    CadastroComponent e LoginComponent Completos - Implementações: login funcional, bootstrap 5, reactive forms,  validação de e-mail, validação de senha, integração com backend, authService integrado, persistência em localStorage, navegação automática

--- sistema-palestras/src/app/cadastro/cadastro.component.ts
+++ sistema-palestras/src/app/cadastro/cadastro.component.ts
@@ -1,9 +1,117 @@
-import { Component } from '@angular/core';
+import {
+ Component
+} from '@angular/core';
+
+import {
+ FormBuilder,
+ FormGroup,
+ Validators,
+ ReactiveFormsModule
+} from '@angular/forms';
+
+import {
+ Router,
+ RouterLink
+} from '@angular/router';
+
+import {
+ CommonModule
+} from '@angular/common';
+
+import {
+ ApiAuthService
+} from '../services/api-auth.service';
 
 @Component({
-  selector: 'app-cadastro',
-  imports: [],
-  templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css',
+ selector:'app-cadastro',
+ standalone:true,
+ imports:[
+   CommonModule,
+   ReactiveFormsModule,
+   RouterLink
+ ],
+ templateUrl:'./cadastro.component.html'
 })
-export class Cadastro {}
+export class CadastroComponent {
+
+ cadastroForm!:FormGroup;
+
+ mensagem:string='';
+
+ erro:boolean=false;
+
+ constructor(
+   private fb:FormBuilder,
+   private api:ApiAuthService,
+   private router:Router
+ ){
+
+   this.cadastroForm =
+     this.fb.group({
+
+       nome:[
+         '',
+         [
+          Validators.required,
+          Validators.minLength(3)
+         ]
+       ],
+
+       email:[
+         '',
+         [
+          Validators.required,
+          Validators.email
+         ]
+       ],
+
+       senha:[
+         '',
+         [
+          Validators.required,
+          Validators.minLength(8)
+         ]
+       ]
+     });
+ }
+
+ cadastrar(){
+
+   if(
+     this.cadastroForm.invalid
+   ){
+     return;
+   }
+
+   this.api
+   .cadastrar(
+     this.cadastroForm.value
+   )
+   .subscribe({
+
+     next:()=>{
+
+       this.erro=false;
+
+       this.mensagem=
+       'Cadastro realizado com sucesso';
+
+       setTimeout(()=>{
+
+         this.router.navigate(
+           ['/login']
+         );
+
+       },1500);
+     },
+
+     error:(err)=>{
+
+       this.erro=true;
+
+       this.mensagem=
+       err.error.message;
+     }
+   });
+ }
+}
--- sistema-palestras/src/app/login/login.component.ts
+++ sistema-palestras/src/app/login/login.component.ts
@@ -1,9 +1,100 @@
-import { Component } from '@angular/core';
+import {
+ Component, 
+ OnInit
+} from '@angular/core';
+
+import {
+ FormBuilder,
+ FormGroup,
+ Validators,
+ ReactiveFormsModule
+} from '@angular/forms';
+
+import {
+ Router,
+ RouterLink
+} from '@angular/router';
+
+import {
+ CommonModule
+} from '@angular/common';
+
+import {
+ ApiAuthService
+} from '../services/api-auth.service';
+
+import {
+ AuthService
+} from '../services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+ selector:'app-login',
+ standalone:true,
+ imports:[
+   CommonModule,
+   ReactiveFormsModule,
+   RouterLink
+ ],
+ templateUrl:'./login.component.html'
 })
-export class Login {}
+export class LoginComponent {
+
+ loginForm!:FormGroup;
+ mensagem:string='';
+ erro:boolean=false;
+
+ constructor(
+   private fb:FormBuilder,
+   private api:ApiAuthService,
+   private auth:AuthService,
+   private router:Router
+ ){
+
+   this.loginForm=
+   this.fb.group({
+
+     email:[
+       '',
+       [
+        Validators.required,
+        Validators.email
+       ]
+     ],
+
+     senha:[
+       '',
+       Validators.required
+     ]
+   });
+ }
+
+ entrar(){
+
+   if(
+     this.loginForm.invalid
+   ){
+     return;
+   }
+
+   this.api.login(
+     this.loginForm.value
+   )
+   .subscribe({
+     next:(response:any)=>{
+       this.auth.login(
+         response.userData
+       );
+
+       this.router.navigate(
+         ['/home']
+       );
+     },
+
+     error:(err)=>{
+       this.erro=true;
+       this.mensagem=
+       err.error.message;
+     }
+   });
+ }
+}
